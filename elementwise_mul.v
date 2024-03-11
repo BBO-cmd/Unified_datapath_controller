0febc748dfd18c0f4d6a629aec77e6365fc131fb
Boryoung Kim <80330470+BBO-cmd@users.noreply.github.com>
2024-03-11 19:51:41 +0900

    Update elementwise_mul.v
    Simulation Succeed

--- elementwise_mul.v
+++ elementwise_mul.v
@@ -1,54 +1,56 @@
 `timescale 1ns / 1ps
 
-module elementwise_mul(
-    input clk,
-    input rstn,
-    input [127:0] U_entry, // 4x4 mtx in 1D tensor(len=16)
-    input [127:0] V_entry, // 4x4 mtx in 1D tensor(len=16)
-    output [127:0] M       // 4x4 mtx in 1D tensor(len=16) 
+
+
+module elementwise_mul #(parameter W=8)
+(
+    input clk, 
+    input rstn, 
+    input [16*W-1:0] i_mtx_u,   // 4x4 mtx in 1D tensor(len=16)
+    input [16*W-1:0] i_mtx_v,   // 4x4 mtx in 1D tensor(len=16)
+    output [16*W*2-1:0] o_mtx_m // 4x4 mtx in 1D tensor(len=16), each element = 16bit
 );
 
 
-
 //internal signals
-wire[15:0] y00;
-wire[15:0] y01;
-wire[15:0] y02;
-wire[15:0] y03;
-wire[15:0] y04;
-wire[15:0] y05;
-wire[15:0] y06;
-wire[15:0] y07;
-wire[15:0] y08;
-wire[15:0] y09;
-wire[15:0] y10;
-wire[15:0] y11;
-wire[15:0] y12;
-wire[15:0] y13;
-wire[15:0] y14;
-wire[15:0] y15;
+wire[2*W-1:0] y00; 
+wire[2*W-1:0] y01; 
+wire[2*W-1:0] y02; 
+wire[2*W-1:0] y03;
+wire[2*W-1:0] y04;
+wire[2*W-1:0] y05;
+wire[2*W-1:0] y06;
+wire[2*W-1:0] y07;
+wire[2*W-1:0] y08;
+wire[2*W-1:0] y09;
+wire[2*W-1:0] y10;
+wire[2*W-1:0] y11;
+wire[2*W-1:0] y12;
+wire[2*W-1:0] y13;
+wire[2*W-1:0] y14;
+wire[2*W-1:0] y15;
 
 
 //Doing elementwise multiplication
-assign y00 = U_entry[7:0] * V_entry[7:0];
-assign y01 = U_entry[15:8] * V_entry[15:8];
-assign y02 = U_entry[23:16] * V_entry[23:16];
-assign y03 = U_entry[31:24] * V_entry[31:24];
-assign y04 = U_entry[39:32] * V_entry[39:32];
-assign y05 = U_entry[47:40] * V_entry[47:40];
-assign y06 = U_entry[55:48] * V_entry[55:48];
-assign y07 = U_entry[63:56] * V_entry[63:56];
-assign y08 = U_entry[71:64] * V_entry[71:64];
-assign y09 = U_entry[79:72] * V_entry[79:72];
-assign y10 = U_entry[87:80] * V_entry[87:80];
-assign y11 = U_entry[95:88] * V_entry[95:88];
-assign y12 = U_entry[103:96] * V_entry[103:96];
-assign y13 = U_entry[111:104] * V_entry[111:104];
-assign y14 = U_entry[119:112] * V_entry[119:112];
-assign y15 = U_entry[127:120] * V_entry[127:120];
+assign y00 = i_mtx_u[0*W +: W] * i_mtx_v[0*W +: W];
+assign y01 = i_mtx_u[1*W +: W] * i_mtx_v[1*W +: W];
+assign y02 = i_mtx_u[2*W +: W] * i_mtx_v[2*W +: W];
+assign y03 = i_mtx_u[3*W +: W] * i_mtx_v[3*W +: W];
+assign y04 = i_mtx_u[4*W +: W] * i_mtx_v[4*W +: W];
+assign y05 = i_mtx_u[5*W +: W] * i_mtx_v[5*W +: W];
+assign y06 = i_mtx_u[6*W +: W] * i_mtx_v[6*W +: W];
+assign y07 = i_mtx_u[7*W +: W] * i_mtx_v[7*W +: W];
+assign y08 = i_mtx_u[8*W +: W] * i_mtx_v[8*W +: W];
+assign y09 = i_mtx_u[9*W +: W] * i_mtx_v[9*W +: W];
+assign y10 = i_mtx_u[10*W +: W] * i_mtx_v[10*W +: W];
+assign y11 = i_mtx_u[11*W +: W] * i_mtx_v[11*W +: W];
+assign y12 = i_mtx_u[12*W +: W] * i_mtx_v[12*W +: W];
+assign y13 = i_mtx_u[13*W +: W] * i_mtx_v[13*W +: W];
+assign y14 = i_mtx_u[14*W +: W] * i_mtx_v[14*W +: W];
+assign y15 = i_mtx_u[15*W +: W] * i_mtx_v[15*W +: W];
 
 
-assign M = {y00, y01, y02, y03, y04, y05, y06, y07, y08, y09, y10, y11, y12, y13, y14, y15};
+assign o_mtx_m = {y15, y14, y13, y12, y11, y10, y09, y08, y07, y06, y05, y04, y03, y02, y01, y00};
 
 
 endmodule
