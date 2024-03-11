a89f837b0e0922a350c62775eae8ef9ddceac7ee
Boryoung Kim <80330470+BBO-cmd@users.noreply.github.com>
2024-03-11 19:53:25 +0900

    Update elementwise_mul_tb.v
    Simulation succeed

--- elementwise_mul_tb.v
+++ elementwise_mul_tb.v
@@ -1,24 +1,25 @@
 `timescale 1ns / 1ps
-
 module elementwise_mul_tb;
+parameter W=8;
+
     reg clk;
     reg rstn;
-    reg [127:0] U_entry; // 4x4 mtx in 1D tensor(len=16)
-    reg [127:0] V_entry; // 4x4 mtx in 1D tensor(len=16)
-    //output reg [127:0] M;       // 4x4 mtx in 1D tensor(len=16) 
+    reg [16*W-1:0] i_mtx_u; // 4x4 mtx in 1D tensor(len=16)
+    reg [16*W-1:0] i_mtx_v; // 4x4 mtx in 1D tensor(len=16)
+    wire [16*W-1:0] o_mtx_m; // 4x4 mtx in 1D tensor(len=16), each element = 16bit
 //-------------------------------------------
 // DUT: Elementwise multiplier
 //-------------------------------------------
 elementwise_mul u_elementwise_mul(
 .clk(clk),
 .rstn(rstn),
-.U_entry(U_entry),
-.V_entry(V_entry),
-.M(M)
+.i_mtx_u(i_mtx_u),
+.i_mtx_v(i_mtx_v),
+.o_mtx_m(o_mtx_m)
 );
 
 
-// Clock
+// Clock generation 
 parameter CLK_PERIOD = 10;	//100MHz
 initial begin
 	clk = 1'b1;
@@ -28,67 +29,65 @@ end
 // Test cases
 initial begin
 
-    rstn = 1'b0;			// Reset, low active
-    U_entry = 128'd0;
-    V_entry = 128'd0;
-    #(4*CLK_PERIOD) rstn = 1'b1;
+    rstn <= 1'b0;			
+    i_mtx_u <= 128'd0;       
+    i_mtx_v <= 128'd0;
+    #(4*CLK_PERIOD) rstn <= 1'b1;
 
 
-    U_entry[7:0] = 8'd1;
-    V_entry[7:0] = 8'd1;
+    i_mtx_u[7:0] <= 8'd1;
+    i_mtx_v[7:0] <= 8'd1;
 
-    U_entry[15:8] = 8'd2;
-    V_entry[15:8] = 8'd2;
+    i_mtx_u[15:8] <= 8'd2;
+    i_mtx_v[15:8] <= 8'd2;
 
-    U_entry[23:16] = 8'd3;
-    V_entry[23:16] = 8'd3;
+    i_mtx_u[23:16] <= 8'd3;
+    i_mtx_v[23:16] <= 8'd3;
 
-    U_entry[31:24] = 8'd4;
-    V_entry[31:24] = 8'd4;
+    i_mtx_u[31:24] <= 8'd4;
+    i_mtx_v[31:24] <= 8'd4;
 
-    U_entry[39:32] = 8'd5;
-    V_entry[39:32] = 8'd5;
+    i_mtx_u[39:32] <= 8'd1;
+    i_mtx_v[39:32] <= 8'd1;
 
-    U_entry[47:40] = 8'd6;
-    V_entry[47:40] = 8'd6;
+    i_mtx_u[47:40] <= 8'd2;
+    i_mtx_v[47:40] <= 8'd2;
 
-    U_entry[55:48] = 8'd7;
-    V_entry[55:48] = 8'd7;
+    i_mtx_u[55:48] <= 8'd3;
+    i_mtx_v[55:48] <= 8'd3;
 
-    U_entry[63:56] = 8'd8;
-    V_entry[63:56] = 8'd8;
+    i_mtx_u[63:56] <= 8'd4;
+    i_mtx_v[63:56] <= 8'd4;
 
-    U_entry[71:64] = 8'd9;
-    V_entry[71:64] = 8'd9;
+    i_mtx_u[71:64] <= 8'd1;
+    i_mtx_v[71:64] <= 8'd1;
 
-    U_entry[79:72] = 8'd10;
-    V_entry[79:72] = 8'd10;
+    i_mtx_u[79:72] <= 8'd2;
+    i_mtx_v[79:72] <= 8'd2;
 
-    U_entry[87:80] = 8'd11;
-    V_entry[87:80] = 8'd11;
+    i_mtx_u[87:80] <= 8'd3;
+    i_mtx_v[87:80] <= 8'd3;
 
-    U_entry[95:88] = 8'd12;
-    V_entry[95:88] = 8'd12;
+    i_mtx_u[95:88] <= 8'd4;
+    i_mtx_v[95:88] <= 8'd4;
 
-    U_entry[103:96] = 8'd13;
-    V_entry[103:96] = 8'd13;
+    i_mtx_u[103:96] <= 8'd1;
+    i_mtx_v[103:96] <= 8'd1;
 
-    U_entry[111:104] = 8'd14;
-    V_entry[111:104] = 8'd14;
+    i_mtx_u[111:104] <= 8'd2;
+    i_mtx_v[111:104] <= 8'd2;
 
-    U_entry[119:112] = 8'd15;
-    V_entry[119:112] = 8'd15;
+    i_mtx_u[119:112] <= 8'd3;
+    i_mtx_v[119:112] <= 8'd3;
 
-    U_entry[127:120] = 8'd16;
-    V_entry[127:120] = 8'd16;
+    i_mtx_u[127:120] <= 8'd4;
+    i_mtx_v[127:120] <= 8'd4;
     
 
-
-    
-    #(CLK_PERIOD) 
-	@(posedge clk) 		
-        U_entry = 128'd0;
-        V_entry = 128'd0;
+    #(CLK_PERIOD)
+	@(posedge clk)
+        i_mtx_u <= 128'd0;
+        i_mtx_v <= 128'd0;
   		   
 end
 endmodule
