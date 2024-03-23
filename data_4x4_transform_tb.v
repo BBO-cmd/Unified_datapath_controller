63947f2069b07386383834f22cc0bebf54045434
Boryoung Kim <80330470+BBO-cmd@users.noreply.github.com>
2024-03-23 19:09:25 +0900

    register ver: reg B_Td

--- data_4x4_transform_tb.v
+++ data_4x4_transform_tb.v
@@ -6,15 +6,17 @@ parameter W=8;
 reg clk;
 reg rstn;
 reg [127:0] data;
-wire [127:0] data_transformed;
+wire [W*16-1:0] data_transformed;
+
 
 //DUT: data_4x4_transform
 data_4x4_transform u_data_4x4_transform(
     .clk(clk),
     .rstn(rstn),
     .data(data),
-    .data_transformed(data_transformed)
+    .data_out(data_transformed)
 );
+
 
 //clock generation
 parameter CLK_PERIOD = 10;
@@ -30,12 +32,12 @@ end
         data <= 128'd0;
         #(4*CLK_PERIOD) rstn <= 1'b1;
 
-        data = 128'h01010101020202020303030304040404;
+        data = 128'h04040404030303030202020201010101;
 
         #(4*CLK_PERIOD)
         rstn <= 1'b0;
-
     end
+
 
 endmodule
 
