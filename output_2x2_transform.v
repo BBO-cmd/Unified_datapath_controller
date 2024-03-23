028ed336cda2eec433b7fb626ec7c75a8de26f4e
Boryoung Kim <80330470+BBO-cmd@users.noreply.github.com>
2024-03-23 21:51:14 +0900

    reg version_reg: A_TM

--- output_2x2_transform.v
+++ output_2x2_transform.v
@@ -8,21 +8,40 @@ module output_2x2_transform #(parameter W=16)(
     output [63:0] Y //2z2
 );
 
-//A_transpose M A = Y_2x2
-assign Y[0*W +: W] = M[0*W +: W] + M[1*W +: W] + M[2*W +: W] 
-                   + M[4*W +: W] + M[5*W +: W] + M[6*W +: W]
-                   + M[8*W +: W] + M[9*W +: W] + M[10*W +: W];
+reg [W*8-1:0] A_TM;
+reg [W*4-1:0] output_transformed;
 
-assign Y[1*W +: W] = M[1*W +: W] - M[2*W +: W] - M[3*W +: W] 
-                   + M[5*W +: W] - M[6*W +: W] - M[7*W +: W]
-                   + M[9*W +: W] - M[10*W +: W] - M[11*W +: W];
+always @(posedge clk, negedge rstn) begin
+        
+        if (!rstn) begin
+            A_TM <= 0;
+            output_transformed <= 0;
+        end
 
-assign Y[2*W +: W] = M[4*W +: W] + M[5*W +: W] + M[6*W +: W] 
-                   - M[8*W +: W] - M[9*W +: W] - M[10*W +: W]
-                   - M[12*W +: W] - M[13*W +: W] - M[14*W +: W];
+        else begin            
 
-assign Y[3*W +: W] = M[5*W +: W] - M[6*W +: W] - M[7*W +: W] 
-                   - M[9*W +: W] + M[10*W +: W] + M[11*W +: W]
-                   - M[13*W +: W] + M[14*W +: W] + M[15*W +: W];
+            //A_TM
+            A_TM[0*W +: W] <= M[0*W +: W] + M[4*W +: W] + M[8*W +: W];
+            A_TM[1*W +: W] <= M[1*W +: W] + M[5*W +: W] + M[9*W +: W];
+            A_TM[2*W +: W] <= M[2*W +: W] + M[6*W +: W] + M[10*W +: W];
+            A_TM[3*W +: W] <= M[3*W +: W] + M[7*W +: W] + M[11*W +: W];
+
+
+            A_TM[4*W +: W] <= M[4*W +: W] - M[8*W +: W] - M[12*W +: W];
+            A_TM[5*W +: W] <= M[5*W +: W] - M[9*W +: W] - M[13*W +: W];
+            A_TM[6*W +: W] <= M[6*W +: W] - M[10*W +: W] - M[14*W +: W];
+            A_TM[7*W +: W] <= M[7*W +: W] - M[11*W +: W] - M[15*W +: W];
+
+
+            //A_transpos M A = Y 2x2
+            output_transformed[0*W +: W] <= A_TM[0*W +: W] + A_TM[1*W +: W] + A_TM[2*W +: W];
+            output_transformed[1*W +: W] <= A_TM[1*W +: W] - A_TM[2*W +: W] - A_TM[3*W +: W];
+            output_transformed[2*W +: W] <= A_TM[4*W +: W] + A_TM[5*W +: W] + A_TM[6*W +: W];
+            output_transformed[3*W +: W] <= A_TM[5*W +: W] - A_TM[6*W +: W] - A_TM[7*W +: W];
+
+        end
+    end
+
+assign Y = output_transformed;
 
 endmodule
